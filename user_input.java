2a827c98e370f61bb286166f3fa55f16719b0c08
Ananya H R <ananyamonisha123@gmail.com>
2026-02-10 15:38:05 +0530

    complete

---
+++ user_input.java
@@ -0,0 +1,23 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class user_input_double{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double value = scn.nextdouble();
+        System.out.println("The double of is: "+double);
+    }
+}
+
+
+class addition_of_two_numbers{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = scn.nextInt();
+        System.out.println("Enter second number: ");
+        int b = scn.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of "+a+" and "+b+" is: "+sum);
+    }
+}
