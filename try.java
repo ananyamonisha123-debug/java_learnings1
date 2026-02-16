cc8419975ce726071643c5f34d91f7f62959eff4
Ananya H R <ananyamonisha123@gmail.com>
2026-02-16 15:06:09 +0530

    complete

---
+++ try.java
@@ -0,0 +1,95 @@
+class A extends Thread {
+  public void run() {
+    for (int i = 0; i < 50; i++) {
+      System.out.println("Hey Hello: ");
+       try{
+           Thread.sleep(10);
+       }catch(InterruptedException e){
+           System.out.println(e);
+      }
+    }
+  }
+}
+
+class B extends Thread {
+  public void run() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("okay Bye ");
+      setPriority(10);
+       try{
+           Thread.sleep(10);
+       }catch(InterruptedException e){
+           System.out.println(e);}
+    }
+  }
+}
+
+class try1 {
+    public static void main(String[] args) {
+        A threadA = new A();
+        B threadB = new B();
+        
+        // threadB.setPriority(10); // set thread to minimum priority
+        threadA.start();
+        threadB.start();
+    }
+}
+
+// Lamda expression
+class try2{
+    public static void main(String[] args) {
+        Runnable r1 = new Runnable() {
+            public void run() {
+                System.out.println("Hello");
+            }
+        };
+        Thread t1 = new Thread(r1);
+        t1.start();
+    }
+}
+
+// After Lamda expression basically after java 8
+class try3{
+    public static void main(String[] args) {
+        Runnable r1 = () -> {
+            System.out.println("Hello");
+        };
+        Thread t1 = new Thread(r1);
+        t1.start();
+    }
+}
+
+// Race Condition
+class try4{
+    int count = 0;
+    public void increment() {
+        count++;
+    }
+}
+
+
+class try5 {
+    public static void main(String[] args) throws InterruptedException {
+        try4 t = new try4();
+        Thread t1 = new Thread() {
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    t.increment();
+                }
+            }
+        };
+        Thread t2 = new Thread() {
+            public void run() {
+                for (int i = 0;i < 1000; i++) {
+                    t.increment();
+                }
+            }
+        };
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        System.out.println(t.count);
+    }
+}
